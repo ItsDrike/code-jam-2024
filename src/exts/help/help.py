9b3b159b80e69ff0148ff0f494ee873df2d42322
Benjiguy <binyamin1bbb@gmail.com>
2024-07-19 20:29:49 +0300

    added cats to help commands and improved it

--- src/exts/help/help.py
+++ src/exts/help/help.py
@@ -1,8 +1,11 @@
-from discord import ApplicationContext, Bot, Cog, slash_command
+import aiohttp
+from discord import ApplicationContext, Bot, Cog, Embed, slash_command
 
+from src.utils import mention_command
 from src.utils.log import get_logger
 
 log = get_logger(__name__)
+CAT_URL: str = "https://api.thecatapi.com/v1/images/search"
 
 
 class HelpCog(Cog):
@@ -14,7 +17,19 @@ class HelpCog(Cog):
     @slash_command()
     async def help(self, ctx: ApplicationContext) -> None:
         """Help command shows available commands."""
-        await ctx.respond(" *hello* ")
+        async with aiohttp.ClientSession().get(CAT_URL) as response:
+            response.raise_for_status()
+            data = await response.json()
+            url: str = data[0]["url"]
+
+        embed = Embed(
+            title="Help command",
+            image=url,
+        )
+        embed.add_field(name=mention_command("ping", self.bot), value="sends a response with pong", inline=False)
+        embed.add_field(name=mention_command("help", self.bot), value="gives a list of available commands for users")
+        embed.add_field(name="", value="")
+        await ctx.respond(embed=embed)
 
 
 def setup(bot: Bot) -> None:
