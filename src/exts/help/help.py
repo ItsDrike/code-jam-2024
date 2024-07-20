7be9a8c8412840ec561b42d15e008d6013c1d213
Benjiguy <binyamin1bbb@gmail.com>
2024-07-20 21:50:39 +0300

    fixed and added back cat images

--- src/exts/help/help.py
+++ src/exts/help/help.py
@@ -1,3 +1,4 @@
+import aiohttp
 import discord
 from discord import ApplicationContext, Bot, CheckFailure, Cog, SlashCommand, SlashCommandGroup, slash_command
 from discord.ext.commands import CheckFailure as CommandCheckFailure
@@ -20,6 +21,16 @@ class HelpCog(Cog):
         """Shows help for all available commands."""
         fields: list[tuple[str, str]] = []
 
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get("https://api.thecatapi.com/v1/images/search") as resp,
+        ):
+            if resp.status == 200:
+                data = await resp.json()
+                cat_image_url = data[0]["url"]
+            else:
+                cat_image_url = None
+
         for command in self.bot.commands:
             try:
                 can_run = await command.can_run(ctx)
@@ -39,11 +50,13 @@ class HelpCog(Cog):
                     )
                 )
                 fields.append((f"{mention_command(command)} group", value))
-        new_embed = lambda: discord.Embed(title="help command")
-        embeds: list[discord.Embed] = [new_embed()]
+
+        new_embed = lambda url: discord.Embed(title="help command").set_image(url=url)
+
+        embeds: list[discord.Embed] = [new_embed(cat_image_url)]
         for name, value in fields:
             if len(embeds[-1].fields) >= 5:
-                embeds.append(new_embed())
+                embeds.append(new_embed(cat_image_url))
             embeds[-1].add_field(name=name, value=value, inline=False)
         paginator = Paginator([Page(embeds=[embed]) for embed in embeds])
         await paginator.respond(ctx.interaction)
