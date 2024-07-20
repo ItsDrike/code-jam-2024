614b0306a50b7547a65251024d1d5491b19d0b97
Benjiguy <binyamin1bbb@gmail.com>
2024-07-20 22:53:13 +0300

    changed code as requested and added gif support

--- src/utils/cat_api.py
+++ src/utils/cat_api.py
@@ -1,14 +1,14 @@
 import aiohttp
 
+CAT_API_URL = "https://api.thecatapi.com/v1/images/search"
 
-async def get_cat_image_url() -> None:
-    """Get an image of a random cat."""
-    async with (
-        aiohttp.ClientSession() as session,
-        session.get("https://api.thecatapi.com/v1/images/search") as resp,
-    ):
-        if resp.status == 200:
-            data = await resp.json()
-            return data[0]["url"]
-        else:  # noqa: RET505
-            return None
+
+async def get_cat_image_url() -> str:
+    """Get a URL for a random cat image.
+
+    The produced image can also be a GIF.
+    """
+    async with aiohttp.ClientSession() as session, session.get(CAT_API_URL) as resp:
+        resp.raise_for_status()
+        data = await resp.json()
+        return data[0]["url"]
